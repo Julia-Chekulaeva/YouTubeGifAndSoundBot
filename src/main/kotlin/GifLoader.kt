d407167b7e82d647814562475bc465f2c1d8c1c2
Julia <julia4eck@gmail.com>
2022-12-19 04:15:32 +0300

    Add exceptions, resources directory path

--- src/main/kotlin/GifLoader.kt
+++ src/main/kotlin/GifLoader.kt
@@ -1,24 +1,45 @@
 import it.grabz.grabzit.GrabzItClient
+import it.grabz.grabzit.GrabzItException
 import it.grabz.grabzit.parameters.AnimationOptions
 import java.io.File
 
+
+const val resources = "src/main/resources/"
+
 val secrets = File("config.yaml").readLines().map { it.split(": ") }.associate { it[0] to it[1] }
 
+class GifTooLargeException(override val message: String?) : Exception()
+
 fun main() {
-    gifLoader("https://youtu.be/gVkyBu_v7vA", 11, 100)
+    gifLoader("https://youtu.be/TtCXUFImZYE", 0, 40)
 }
 
-fun gifLoader(url: String, start: Int, end: Int) {
+fun gifLoader(url: String, start: Int, end: Int): Boolean {
     val client = GrabzItClient(secrets["gif_loader_key"], secrets["gif_loader_secret"])
-
-    val options = AnimationOptions()
-    options.duration = end - start
-    options.start = start
-    options.customId = "123456"
-    println("start=${options.start}, duration=${options.duration}, frPerSec=${options.framesPerSecond}, speed=${
-        options.speed}, h=${options.height}, w=${options.width}, q=${options.quality}")
-
-    client.URLToAnimation(url, options)
-    //Then call the Save method
-    client.Save("http://www.example.com/handler")
+    val animOptions = AnimationOptions()
+    println(
+        "start=${animOptions.start}, duration=${animOptions.duration}, frPerSec=${animOptions.framesPerSecond}, speed=${
+            animOptions.speed
+        }, h=${animOptions.height}, w=${animOptions.width}, q=${animOptions.quality}"
+    )
+    animOptions.duration = end - start
+    animOptions.framesPerSecond = 20F
+    animOptions.speed = 1F
+    animOptions.start = start
+    animOptions.quality = -1
+    println(
+        "start=${animOptions.start}, duration=${animOptions.duration}, frPerSec=${animOptions.framesPerSecond}, speed=${
+            animOptions.speed
+        }, h=${animOptions.height}, w=${animOptions.width}, q=${animOptions.quality}"
+    )
+    try {
+        client.URLToAnimation(url, animOptions)
+        client.SaveTo(resources + "anim.gif")
+        return true
+    } catch (e: GrabzItException) {
+        if (e.message == "Your animated GIF exceeds the maximum total resolution allowed. " +
+            "Reduce the duration, frames per second, width or height of the image to create your animated GIF.")
+            throw GifTooLargeException("The given interval is too big: the video fragment can't be loaded.")
+        throw e
+    }
 }

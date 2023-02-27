c392724de9386241146fc92a846f6d49c7b3ca1a
Julia <julia4eck@gmail.com>
2023-02-27 21:36:22 +0300

    Fix and add debugging

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -10,9 +10,9 @@ class Tests {
     ) {
         val videoInfo = getInfo(url, downloader).data()
         val file = File(fileName)
+        println("Downloading data into file ${file.absolutePath}")
         if (!file.parentFile.exists())
             throw Exception("No parent directory for file ${file.absolutePath}")
-        println("Downloading data into file ${file.absolutePath}")
         loader(start, end, fileName, videoInfo, cmdId)
         check(file.exists())
         file.delete()
@@ -40,8 +40,8 @@ class Tests {
     fun testVideoLoading() {
         val downloader = YoutubeDownloader()
         testLoader("https://www.youtube.com/watch?v=NrJEFrth27Q", downloader,
-            0, 2, "file0_10.mp4", 4)
+            0, 2, "src/test/resources/file0_10.mp4", 4)
         testLoader("https://www.youtube.com/watch?v=NrJEFrth27Q", downloader,
-            140, 144, "file140_160.mp4", 4)
+            140, 144, "src/test/resources/file140_160.mp4", 4)
     }
 }
